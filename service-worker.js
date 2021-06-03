2729a01b14d87f37f12877d5147f45183c74e173
Offor <offorb23@gmail.com>
2021-06-03 12:59:57 +0400

    changes and service worker added

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/product-fullsize.jpg',
+    'images/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+
+        })
+    );
+});
+
